530b441ee66baea93b312d142531931b3a7a4b98
jeyhooon <jeyhoon.maskani@gmail.com>
2023-10-21 10:46:32 +0200

    UPDATE: implemented sensorUpdate method (Resampling Wheel method to resample particles based on their importance) | REFACTORED: moved helper functions to helper_func.cpp

--- main.cpp
+++ main.cpp
@@ -3,48 +3,17 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <math.h>
-#include <vector>
-#include <random>
 #include "robot_defs.h"
 #include "controller.h"
 #include "main.h"
 
 // Defining global variables
+RobotParams rParams; 
 RobotState currentEstimate;   // Current robot position estimate
-std::vector<FieldLocation> landmarks; // Global location of each landmark
-std::vector<RobotState> particles;
-static std::default_random_engine randEngine(std::random_device{}());
-std::uniform_real_distribution<double> randRealDist(-1.0, 1.0);
-
-// Helper functions
-double pdfGaussian(double mu, double sigma, double x)
-{
-    // Probability of x for 1-dim Gaussian with mean mu and stddev sigma
-    return exp(-(pow((mu - x), 2)) / (pow(sigma, 2)) / 2.0) / sqrt(2.0 * M_PI * (pow(sigma, 2)));
-}
-
-void updateCurrentEstimate()
-{
-    
-
-    // only update the current estimate if there are particles
-    if(!particles.empty())
-    {
-        // use mean values of particles as current estimate
-        double sumX = 0, sumY = 0, sumTheta = 0;
-        for (auto& p : particles) {
-            sumX += p.x;
-            sumY += p.y;
-            sumTheta += p.theta;
-        }
-
-        double numParticles = static_cast<double>(particles.size());
-        currentEstimate.x = sumX/numParticles;
-        currentEstimate.y = sumY/numParticles;
-        currentEstimate.theta = sumTheta/numParticles;
-    }
-}
+std::vector<FieldLocation> landmarks(NUM_LANDMARKS);  // Global location of each landmark
+std::vector<RobotState> particles(MAX_NUM_PARTICLES); // Particles for MCL algorithm
+std::vector<RobotState> newParticles(MAX_NUM_PARTICLES);
+std::vector<double> weights(MAX_NUM_PARTICLES);       // Importance weights for each particle
 
 /**
  * getRobotPositionEstimate()
@@ -84,7 +53,7 @@ void motionUpdate(RobotState delta)
     }
 
     // finally, updating the current estimate based on the updated particles
-    updateCurrentEstimate();
+    updateCurrentEstimate(currentEstimate, particles, weights);
 }
 
 /**
@@ -97,6 +66,68 @@ void motionUpdate(RobotState delta)
 void sensorUpdate(std::vector<MarkerObservation> observations)
 {
     // TODO: Write your sensor update procedures here
+    std::cout << "Landmark index: " << observations[0].markerIndex;
+    std::cout <<" x: " << landmarks[observations[0].markerIndex].x <<  " y: " << landmarks[observations[0].markerIndex].y << std::endl;
+
+    if (!observations.empty() && !particles.empty())
+    {
+        // calculate particle importance weights
+        double maxWeight = -1;
+        double Neff = 0;
+        updateWeights(weights, particles, observations, landmarks, rParams, maxWeight, Neff);
+
+        int N = particles.size();
+        int N_resample = (1 - ALPHA) * N;
+        int N_random = N - N_resample;
+
+        //  if effective number of particles is too low, resample (lack of diversity)
+        if(Neff < RESAMPLING_THRESHOLD * static_cast<double>(N))
+        {
+            std::cout << "Resampling..." << std::endl;
+
+            //Resample the particles using Resampling Wheel technique
+            newParticles.clear();
+            std::uniform_real_distribution<double> randRealDist(0, 1.0);
+            double beta = 0;
+            int index = randRealDist(randEngine) * N;
+
+            std::normal_distribution<double> noiseDist(0.0, 0.005);
+            for (int i = 0; i < N_resample; i++)
+            {
+                beta += randRealDist(randEngine) * 2 * maxWeight;
+                while(beta > weights[index])
+                {
+                    beta -= weights[index];
+                    index = (index + 1) % N;
+                }
+                RobotState p;
+                p.x = particles[index].x + noiseDist(randEngine);
+                p.y = particles[index].y + noiseDist(randEngine);
+                p.theta = particles[index].theta + noiseDist(randEngine);
+                newParticles.push_back(p);
+            }
+
+            // Add random particles
+            // std::uniform_real_distribution<double> randParticleDist(-1.0, 1.0);
+            // for (int i = 0; i < N_random; ++i) {
+            //     RobotState p;
+            //     p.x = randParticleDist(randEngine) * (METERS_PER_PIXEL * (FIELD_LENGTH/2 + FIELD_OFFSET_X));
+            //     p.y = randParticleDist(randEngine) * (METERS_PER_PIXEL * (FIELD_WIDTH/2 + FIELD_OFFSET_Y));
+            //     p.theta = randParticleDist(randEngine) * M_PI;
+            //     newParticles.push_back(p);
+            // }
+
+            particles = newParticles;
+
+            // recomputing weights
+
+            
+            // finally, updating the current estimate based on the updated particles
+            updateCurrentEstimate(currentEstimate, particles, weights);
+        }        
+
+        
+    }
     
 }
 
@@ -110,25 +141,51 @@ void myinit(RobotState robotState, RobotParams robotParams,
             FieldLocation markerLocations[NUM_LANDMARKS])
 {
     // TODO: Write your initialization procedures here
+    rParams = robotParams;
     currentEstimate = robotState;
 
     // pre-allocate memory
     landmarks.reserve(NUM_LANDMARKS);
     particles.reserve(MAX_NUM_PARTICLES);
+    newParticles.reserve(MAX_NUM_PARTICLES);
+    weights.reserve(MAX_NUM_PARTICLES);
 
     // store landmarks
     for (int i = 0; i < NUM_LANDMARKS; i++) {
-        landmarks.push_back(markerLocations[i]);
+        landmarks[i] = markerLocations[i];
     }
 
-    // initialize particles uniformly distributed
+    // initialize particles uniformly distributed on the field
+    std::uniform_real_distribution<double> randRealDist(-1.0, 1.0);
+
     for (int i = 0; i < MAX_NUM_PARTICLES; i++) {
-        RobotState particle;
-        particle.x = randRealDist(randEngine) * (METERS_PER_PIXEL * (FIELD_LENGTH/2 + FIELD_OFFSET_X));
-        particle.y = randRealDist(randEngine) * (METERS_PER_PIXEL * (FIELD_WIDTH/2 + FIELD_OFFSET_Y));
-        particle.theta = randRealDist(randEngine) * M_PI;
-        particles.push_back(particle);
+        RobotState p;
+        p.x = randRealDist(randEngine) * (METERS_PER_PIXEL * (FIELD_LENGTH/2 + FIELD_OFFSET_X));
+        p.y = randRealDist(randEngine) * (METERS_PER_PIXEL * (FIELD_WIDTH/2 + FIELD_OFFSET_Y));
+        p.theta = randRealDist(randEngine) * M_PI;
+        particles[i] = p;
     }
+
+    // initialize particles using Gaussian distribution around initial position (instead of uniform distribution on the field)
+    // (using the same sensor noise values for initial robot state)
+    // std::normal_distribution<double> gaussX(robotState.x, rParams.sensor_noise_distance);
+    // std::normal_distribution<double> gaussY(robotState.y, rParams.sensor_noise_distance);
+    // std::normal_distribution<double> gaussTheta(robotState.theta, rParams.sensor_noise_orientation);
+
+    // for (int i = 0; i < MAX_NUM_PARTICLES; i++) {
+    //     RobotState p;
+    //     p.x = gaussX(randEngine);
+    //     p.y = gaussY(randEngine);
+    //     p.theta = gaussTheta(randEngine);
+
+    //     // Ensure theta stays in [-pi, pi] interval
+    //     if (p.theta > M_PI) p.theta -= 2 * M_PI;
+    //     if (p.theta < -M_PI) p.theta += 2 * M_PI;
+        
+    //     particles[i] = p;
+    // }
+
+    updateCurrentEstimate(currentEstimate, particles, weights);
 
 }
 
